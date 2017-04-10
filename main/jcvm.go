f2202ef2ee4f7e4b34718c40606c9fcaedd1a164
yenden <nadiyahyende@gmail.com>
2017-04-10 14:40:56 +0200

    Test file to test a simple exmpl of incrementation

---
+++ main/jcvm.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"JCVM/core"
+)
+
+func main() {
+	//args := os.Args[0:]
+	/*	if args == nil || len(args) == 0 {
+		fmt.Println("Usage: \n\tjcvm.exe library1.ijc library2.ijc ..... yourApplet.ijc ")
+	} else {*/
+	var i int
+	args := []string{`framework.ijc`, `lang.ijc`, `shortAdd.ijc`}
+	for i = 0; i < len(args)-1; i++ {
+		dataBuffer := core.ReadInBuffer(args[i])
+		core.Lst.PushBack(core.BuildApplet(dataBuffer, len(dataBuffer)))
+	}
+	appletBuffer := core.ReadInBuffer(args[i])
+	capp := core.BuildApplet(appletBuffer, len(appletBuffer))
+	vm := initVM()
+	capp.Install(vm)
+	//}
+}
+func initVM() *core.VM {
+	vm := &core.VM{}
+	vm.FrameTop = -1
+	vm.StackFrame = make([]*core.Frame, 256)
+	f := &core.Frame{}
+	vm.PushFrame(f)
+	return vm
+}
---
+++ main/testParser.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+func main() {
+	//fmt.Println("----CAP2IJC version 1.0 ----")
+	/*args := os.Args[1:]
+	if args == nil || len(args) == 0 {
+		fmt.Println("Error args")
+		return
+	}
+	if len(args) == 1 {
+	baComp := core.ReadInBuffer("shortAdd.ijc")
+	//fmt.Println(baComp)
+	newApp := core.BuildApplet(baComp, len(baComp))
+	if newApp == nil {
+		fmt.Println(" c'est nul")
+
+	} else {
+		abc := newApp.AbsA.PHeader.MinorVersion
+		fmt.Printf("min version %d", abc)
+	}
+	//	}
+
+}*/
