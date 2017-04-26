3316c6ca290c9b255df56c38a40a12b3a02358c0
yenden <nadiyahyende@gmail.com>
2017-04-26 12:22:22 +0200

    Add Testing framework to BuildApp

---
+++ test/testParser.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"JCVM/core"
+	"testing"
+)
+
+func TestBuildApplet(t *testing.T) {
+	var app *core.CardApplet
+	args := []string{`framework.ijc`, `lang.ijc`, `helloword.ijc`}
+	for i := 0; i < len(args)-1; i++ {
+		dataBuffer := core.ReadInBuffer(args[i])
+		app = core.BuildApplet(dataBuffer, len(dataBuffer))
+		if app == nil {
+			t.Errorf("%s parsing return nil", args[i])
+		}
+	}
+
+}
