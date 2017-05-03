b7ab3c7d5dff7b14014d017e92142eec304a0a98
yenden <nadiyahyende@gmail.com>
2017-05-03 15:48:14 +0200

    Add arraycopy non atomic

--- jcre/api/javacard/framework/util.go
+++ jcre/api/javacard/framework/util.go
@@ -1,14 +1,14 @@
 package framework
 
-func ArrayCopy(srcArray []byte, srcoffset int16, destArray []byte, destoffset int16, length int16) int16{
+func ArrayCopy(srcArray []byte, srcoffset int16, destArray []byte, destoffset int16, length int16) int16 {
 	srclen := destoffset + length
 	destlen := srcoffset + length
 	copy(destArray[destoffset:srclen], srcArray[srcoffset:destlen])
-	return destoffset+length
+	return destoffset + length
 }
-func ArrayCopyNonAtomic(src []byte,srcOff int16,dest [],destOff int16,length int16)int16{
+func ArrayCopyNonAtomic(src []byte, srcOff int16, dest []byte, destOff int16, length int16) int16 {
 	srclen := destOff + length
 	destlen := srcOff + length
-	copy(destArray[destOff:srclen], srcArray[srcOff:destlen])
-	return destOff+length
+	copy(dest[destOff:srclen], src[srcOff:destlen])
+	return destOff + length
 }
