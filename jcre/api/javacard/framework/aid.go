d25eb431cddaf3ef9fdc641fe539ddad4f794b99
yenden <nadiyahyende@gmail.com>
2017-05-03 13:25:59 +0200

    Delete bArray from AID struct

--- jcre/api/javacard/framework/aid.go
+++ jcre/api/javacard/framework/aid.go
@@ -2,14 +2,16 @@ package framework
 
 import (
 	"JCVM/jcre/api/com/sun/javacard/impl"
+	"JCVM/jcre/api/share"
 	"log"
 	"reflect"
 )
 
 type AID struct {
 	theAID []byte
-	bArray []byte
 }
+
+var bArray []byte
 
 func InitAID(bArray []byte, offset int16, length int16) *AID /*(*AID, error)*/ {
 	aid := &AID{}
@@ -33,44 +35,44 @@ func (aid *AID) getBytes(dest []byte, offset int16) byte {
 	return byte(len(aid.theAID))
 }
 
-func (aid *AID) equals(object interface{}) bool {
+func (aid *AID) Equals(object interface{}) bool {
 	if object == nil {
 		return false
 	}
-	if aid.bArray == nil {
-		aid.bArray = make([]byte, 16) //TODO JCSYstem.makeTransientByteArray((short) 16, JCSystem.CLEAR_ON_RESET);
+	if bArray == nil {
+		bArray = share.MakeTransientByteArray(16, share.ClearOnReset)
 	}
 	switch object.(type) {
 	case *AID:
-		object.(*AID).getBytes(aid.bArray, 0)
-		return reflect.DeepEqual(aid.bArray, aid.theAID)
+		object.(*AID).getBytes(bArray, 0)
+		return reflect.DeepEqual(bArray, aid.theAID)
 	default:
 		return false
 	}
 }
-func equals(aid *AID, bArray []byte, offset int16, length int16) bool {
+func Equals(aid *AID, bArray []byte, offset int16, length int16) bool {
 	err := impl.CheckArrayArgs(bArray, offset, int16(len(aid.theAID)))
 	if err == nil {
 		log.Fatal(err)
 	}
-	return int16(len(aid.theAID)) == length && reflect.DeepEqual(aid.bArray, aid.theAID)
+	return int16(len(aid.theAID)) == length && reflect.DeepEqual(bArray, aid.theAID)
 }
 func (aid *AID) partialEquals(bArray []byte, offset int16, length int16) bool {
 	err := impl.CheckArrayArgs(bArray, offset, int16(len(aid.theAID)))
 	if err == nil {
 		log.Fatal(err)
 	}
-	return reflect.DeepEqual(aid.bArray, aid.theAID)
+	return reflect.DeepEqual(bArray, aid.theAID)
 }
-func (aid *AID) ridEquals(otherAID *AID) bool {
+func (aid *AID) RidEquals(otherAID *AID) bool {
 	if otherAID == nil {
 		return false
 	}
-	if aid.bArray == nil {
-		aid.bArray = make([]byte, 16) //TODO JCSYstem.makeTransientByteArray((short) 16, JCSystem.CLEAR_ON_RESET);
+	if bArray == nil {
+		bArray = share.MakeTransientByteArray(16, share.ClearOnReset)
 	}
-	otherAID.getBytes(aid.bArray, 0)
-	return reflect.DeepEqual(aid.bArray[:5], aid.theAID[:5])
+	otherAID.getBytes(bArray, 0)
+	return reflect.DeepEqual(bArray[:5], aid.theAID[:5])
 
 }
 func (aid *AID) getPartialBytes(aidOffset int16, dest []byte, oOffset int16, oLength int16) {
