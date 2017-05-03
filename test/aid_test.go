4e0215cad4fe2454735e9ad95f5211bdce899c44
yenden <nadiyahyende@gmail.com>
2017-05-03 13:46:31 +0200

    Add test to aid and jcsystem functions

---
+++ test/aid_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"JCVM/jcre/api/javacard/framework"
+	"testing"
+)
+
+func TestAIDfunctions(t *testing.T) {
+	array := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x00, 0x00, 0x03}
+	array2 := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x01, 0x00}
+	aid := framework.InitAID(array, 0, 8)
+	aid2 := framework.InitAID(array2, 0, 7)
+	if !framework.Equals(aid, array, 0, 8) {
+		t.Error("Erreur on aid, arrays not matched")
+	}
+	cond := framework.Equals(aid, array, 0, 4)
+	if cond {
+		t.Error("Erreur on aids length")
+	}
+	cond2 := aid.RidEquals(aid2)
+	if !cond2 {
+		t.Error("Erreur: RIDs don't match")
+	}
+}
---
+++ test/jcsystem_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"JCVM/jcre/api/share"
+	"reflect"
+	"testing"
+)
+
+func TestMakeTransientArray(t *testing.T) {
+	var array1 []bool
+	var array2 []byte
+	var array3 []int16
+	array1 = share.MakeTransientBooleanArray(4, 1)
+	if reflect.DeepEqual(share.ClearOnResetTransientObjs[share.Kr], array1) == false {
+		t.Error("Erreur on maketransient, array not addded")
+	}
+	array2 = share.MakeTransientByteArray(3, 2)
+	if reflect.DeepEqual(share.ClearOnDeselectTransientObjs[share.Kd], array2) == false {
+		t.Error("Erreur on maketransient, array not addded")
+	}
+	array3 = share.MakeTransientShortArray(1, 1)
+
+	if reflect.DeepEqual(share.ClearOnResetTransientObjs[share.Kr], array3) == false {
+		t.Error("Erreur on maketransient, array not addded")
+	}
+}
